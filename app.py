566c56d20c61031c33dcb5e39b55c3d8e359fa8b
fazilm-ai <fazilmdevops@gmail.com>
2025-02-10 15:31:11 -0500

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello, World! Deployed via CI/CD Pipeline! This is Change H"
+    return "Hello, World! Deployed via CI/CD Pipeline! This is Change I"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
